30d06bda7645a518eed78144a736658961306087
ipratikner <pratiknerlikar@gmail.com>
2021-10-18 14:15:05 +0530

    Added: checking player want to play or stop

--- src/com/bridgelabz/Gamble.java
+++ src/com/bridgelabz/Gamble.java
@@ -1,77 +1,21 @@
 package com.bridgelabz;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Gamble{
 
-    int everyGameBet=1;
-    public void winLosse(){
-        int afterBet=0;
-        Random r1=new Random();
-
-        for (int j=1; j<=12; j++)
-        {
-            int winCount=0;
-            int looseCount=0;
-            int profitAmt=0;
-            int looseAmt=0;
-            int tempEverydayStake=0;
-            System.out.println("**********");
-            System.out.println("MONTH "+j);
-            for (int i=1; i<=30; i++)
-            {
-                tempEverydayStake=afterBet+100;
-                int stakeCheck=tempEverydayStake;
-                System.out.println("bet amount for day "+i+" is:"+tempEverydayStake);
-
-                int fPer=tempEverydayStake/2;
-                int winPer=tempEverydayStake+fPer;
-                int loosePer=tempEverydayStake-fPer;
-
-                while(tempEverydayStake>loosePer && tempEverydayStake<winPer)
-                {
-                    int winLoose=r1.nextInt(2);
-                    if(winLoose==1)
-                    {
-                        //System.out.println("win");
-                        afterBet=tempEverydayStake+everyGameBet;
-                    }
-                    else
-                    {
-                        //System.out.println("loose");
-                        afterBet=tempEverydayStake-everyGameBet;
-                    }
-                    tempEverydayStake=afterBet;
-                    //System.out.println("after play="+afterBet);
-                }
-                System.out.println("-----------------------");
-                System.out.println("day "+i+" after the bet amount:"+tempEverydayStake);
-
-                if(stakeCheck<afterBet)
-                {
-                    int profit=afterBet-stakeCheck;
-                    winCount++;
-                    profitAmt +=profit;
-                    System.out.println("day "+i+" in profit:"+profit);
-                }
-                else
-                {
-                    int loose=stakeCheck-afterBet;
-                    looseCount++;
-                    looseAmt +=loose;
-                    System.out.println("day "+i+" in loose:"+loose);
-                }
-
-            }
-            System.out.println("WIN COUNT FOR MONTH "+j+" is::"+winCount);
-            System.out.println("MONTH "+j+" profit amt:"+profitAmt);
-            System.out.println("LOOSE COUNT FOR MONTH "+j+" is::"+looseCount);
-            System.out.println("MONTH "+j+" loost amt:"+looseAmt);
-        }
-
-    }
     public static void main(String[] args) {
-        Gamble g = new Gamble();
-        g.winLosse();
+
+        System.out.println("Do you want to play??? press 1 for yes and 0 for no");
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+
+        if (a==1){
+            System.out.println("lets play");
+        }
+        else{
+            System.out.println("thanks for playing");
+        }
     }
 }
