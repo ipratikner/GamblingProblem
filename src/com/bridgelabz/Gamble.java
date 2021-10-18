37dc80688eea1acfcf2834e439fc1893f454a888
ipratikner <pratiknerlikar@gmail.com>
2021-10-18 13:57:09 +0530

    Added: calculation for each month added

--- src/com/bridgelabz/Gamble.java
+++ src/com/bridgelabz/Gamble.java
@@ -3,51 +3,70 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Gamble{
-    int tempEverydayStake=100;
+
     int everyGameBet=1;
     public void winLosse(){
         int afterBet=0;
         Random r1=new Random();
 
-        for (int i=1; i<=20; i++)
+        for (int j=1; j<=12; j++)
         {
-            tempEverydayStake=afterBet+100;
-            int stakeCheck=tempEverydayStake;
-            System.out.println("bet amount for day "+i+" is:"+tempEverydayStake);
-
-            int fPer=tempEverydayStake/2;
-            int winPer=tempEverydayStake+fPer;
-            int loosePer=tempEverydayStake-fPer;
-
-            while(tempEverydayStake>loosePer && tempEverydayStake<winPer)
+            int winCount=0;
+            int looseCount=0;
+            int profitAmt=0;
+            int looseAmt=0;
+            int tempEverydayStake=0;
+            System.out.println("**********");
+            System.out.println("MONTH "+j);
+            for (int i=1; i<=30; i++)
             {
-                int winLoose=r1.nextInt(2);
-                if(winLoose==1)
+                tempEverydayStake=afterBet+100;
+                int stakeCheck=tempEverydayStake;
+                System.out.println("bet amount for day "+i+" is:"+tempEverydayStake);
+
+                int fPer=tempEverydayStake/2;
+                int winPer=tempEverydayStake+fPer;
+                int loosePer=tempEverydayStake-fPer;
+
+                while(tempEverydayStake>loosePer && tempEverydayStake<winPer)
                 {
-                    //System.out.println("win");
-                    afterBet=tempEverydayStake+everyGameBet;
+                    int winLoose=r1.nextInt(2);
+                    if(winLoose==1)
+                    {
+                        //System.out.println("win");
+                        afterBet=tempEverydayStake+everyGameBet;
+                    }
+                    else
+                    {
+                        //System.out.println("loose");
+                        afterBet=tempEverydayStake-everyGameBet;
+                    }
+                    tempEverydayStake=afterBet;
+                    //System.out.println("after play="+afterBet);
+                }
+                System.out.println("-----------------------");
+                System.out.println("day "+i+" after the bet amount:"+tempEverydayStake);
+
+                if(stakeCheck<afterBet)
+                {
+                    int profit=afterBet-stakeCheck;
+                    winCount++;
+                    profitAmt +=profit;
+                    System.out.println("day "+i+" in profit:"+profit);
                 }
                 else
                 {
-                    //System.out.println("loose");
-                    afterBet=tempEverydayStake-everyGameBet;
+                    int loose=stakeCheck-afterBet;
+                    looseCount++;
+                    looseAmt +=loose;
+                    System.out.println("day "+i+" in loose:"+loose);
                 }
-                tempEverydayStake=afterBet;
-                //System.out.println("after play="+afterBet);
-            }
-            System.out.println("day "+i+" after the bet amount:"+tempEverydayStake);
 
-            if(stakeCheck<afterBet)
-            {
-                int profit=afterBet-stakeCheck;
-                System.out.println("day "+i+" in profit:"+profit);
             }
-            else
-            {
-                int loose=stakeCheck-afterBet;
-                System.out.println("day "+i+" in loose:"+loose);
-            }
-
+            System.out.println("WIN COUNT FOR MONTH "+j+" is::"+winCount);
+            System.out.println("MONTH "+j+" profit amt:"+profitAmt);
+            System.out.println("LOOSE COUNT FOR MONTH "+j+" is::"+looseCount);
+            System.out.println("MONTH "+j+" loost amt:"+looseAmt);
         }
 
     }
