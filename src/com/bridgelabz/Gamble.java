86517aeba4a6f31c2ab8267e00e9b28ee0153249
ipratikner <pratiknerlikar@gmail.com>
2021-10-18 13:49:26 +0530

    Added: checking 50% criteria

--- src/com/bridgelabz/Gamble.java
+++ src/com/bridgelabz/Gamble.java
@@ -3,18 +3,36 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Gamble {
-    int everydayStake = 100;
-    int everyGameBet = 1;
+    int tempEverydayStake=100;
+    int everyGameBet=1;
 
-    public void winLosse() {
+    public void winLosse(){
+        Random r1=new Random();
+        int winCal=everyGameBet;
+        int looseCal=everyGameBet;
+        int fPer=tempEverydayStake/2;
+        int winPer=fPer+tempEverydayStake;
+        int loosePer=tempEverydayStake-fPer;
+
+        while(tempEverydayStake>loosePer && tempEverydayStake<winPer){
+            int check=r1.nextInt(2);
+            if(check==0){
+                //System.out.println("WIN");
+                tempEverydayStake=tempEverydayStake+winCal;
+                //System.out.println("changed amount after a win"+tempEverydayStake);
+            }
+            else{
+                //System.out.println("LOSSE");
+
+                tempEverydayStake=tempEverydayStake-looseCal;
+                //System.out.println("changed amount after a loose"+tempEverydayStake);
+
+            }
+            //System.out.println("earning"+tempEverydayStake);
+        }
+        System.out.println("earning"+tempEverydayStake);
 
 
-        Random r1 = new Random();
-        int check = r1.nextInt(2);
-        if (check == 0) {
-            System.out.println("WIN");
-        } else
-            System.out.println("LOSE");
     }
 
     public static void main(String[] args) {
