02ad722b71ea60a71fefe052e27c8bfa8debad81
ipratikner <pratiknerlikar@gmail.com>
2021-10-18 13:35:10 +0530

    Added: daily stake and bed amount is added

--- src/com/bridgelabz/Gamble.java
+++ src/com/bridgelabz/Gamble.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
 public class Gamble {
-    public static void main(String[] args) {
+    int everydayStake=100;
+    int everyGameBet=1;
 
-        System.out.println("Welcome to Gambling Problem");
+    public static void main(String[] args)
+    {
+        Gamble g = new Gamble();
+
+        System.out.println("The user can start for the day is:"+g.everydayStake+"$");
+        System.out.println("Minimum amount user can bet every game is:"+g.everyGameBet+"$");
+
     }
 }
